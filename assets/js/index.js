a852656c81d2098a6be9a1b4476a1dd4113a4759
Ismael Salas <ismael.salas.csc@gmail.com>
2024-11-01 21:21:20 -0300

    primero

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+function mostrarMensaje(){
+    document.getElementById('text2').style.display = "block";
+    }
+
+function ocultarMensaje(){
+    document.getElementById('text2').style.display = "none";
+    }
+
+function agrandar(){
+    document.getElementById('img').style.width = "100%";
+}
+
+function achicar(){
+    document.getElementById('img').style.width = "20%";
+}
+
+function agrandarLetra(){
+    document.getElementById('caja3').style.fontSize = 25 + "px";
+}
+
+function achicarLetra(){
+    document.getElementById('caja3').style.fontSize = 16 + "px";
+}
